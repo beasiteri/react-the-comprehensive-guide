1804d3c0623343423afbd495039d26a557cf10d0
beasiteri <siteribea@gmail.com>
2026-09-03 14:19:46 +0200

    Fetch books with useEffect and Axios

--- src/components/books/BookList.tsx
+++ src/components/books/BookList.tsx
@@ -1,5 +1,60 @@
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+
+type BookListProps = {
+  id: number;
+  title: string;
+  author: string;
+  isbn: number;
+  rating: number;
+};
+
 function BookList() {
-  return <div>Books</div>;
+  const [books, setBooks] = useState<BookListProps[]>([]);
+  const [error, setError] = useState<Error | null>(null);
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const { data: books } = await axios.get<BookListProps[]>('/books');
+        setBooks(books);
+      } catch (error) {
+        setError(error instanceof Error ? error : new Error('Unknown error'));
+      }
+    })();
+  }, []);
+
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  }
+
+  return (
+    <div>
+      <h1>Books</h1>
+      <table>
+        <thead>
+          <tr>
+            <td>Title</td>
+            <td>Author</td>
+            <td>Isbn</td>
+            <td>Rating</td>
+          </tr>
+        </thead>
+        <tbody>
+          {books.map((book) => {
+            return (
+              <tr key={book.id}>
+                <td>{book.title}</td>
+                <td>{book.author}</td>
+                <td>{book.isbn}</td>
+                <td>{book.rating}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </div>
+  );
 }
 
 export default BookList;
